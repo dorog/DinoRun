b2f0c2e3cdee562846dad7362ad636cd054968a4
Dorogi-Kovács Gábor <dorogi.x@gmail.com>
2023-01-22 02:00:36 +0100

    Add Player Controller for keyboard

---
+++ DinoRun/Assets/Scripts/KeyboardPlayerController.cs
@@ -0,0 +1,89 @@
+using UnityEngine;
+
+public class KeyboardPlayerController : MonoBehaviour
+{
+    [Header("Jump")]
+    [SerializeField]
+    private Rigidbody2D _rigidbody2d;
+    [SerializeField]
+    private float _maxChargingSeconds;
+    [SerializeField]
+    private float _jumpForce;
+    [SerializeField]
+    private string _jumpAnimationTriggerName;
+
+    [Header("Land")]
+    [SerializeField]
+    private BoxCollider2D _groundBoxCollider;
+    [SerializeField]
+    private float _minDistanceFromGround;
+    [SerializeField]
+    private float _minVelocityBeforeLanding;
+    [SerializeField]
+    private Animator _animator;
+    [SerializeField]
+    private string _landAnimationTriggerName;
+
+    private bool isJumped;
+    private float charger;
+
+    private bool isInAir = false;
+
+    private float _groundY;
+
+    private void Awake()
+    {
+        _groundY = _groundBoxCollider.transform.position.y + _groundBoxCollider.offset.y + (_groundBoxCollider.size.y / 2);
+    }
+
+    private void Update()
+    {
+        Charge();
+
+        Jump();
+
+        Land();
+    }
+
+    private void Charge()
+    {
+        if (Input.GetKey(KeyCode.Space) && _maxChargingSeconds > charger)
+        {
+            charger += Time.deltaTime;
+            if (charger > _maxChargingSeconds)
+            {
+                charger = _maxChargingSeconds;
+            }
+        }
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyUp(KeyCode.Space) && !isInAir)
+        {
+            isJumped = true;
+        }
+    }
+
+    private void Land()
+    {
+        if (isInAir && Mathf.Abs(_rigidbody2d.velocity.y) < _minVelocityBeforeLanding && Mathf.Abs(transform.position.y - _groundY) <= _minDistanceFromGround)
+        {
+            isInAir = false;
+            _animator.SetTrigger(_landAnimationTriggerName);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isJumped)
+        {
+            _rigidbody2d.velocity = new Vector2(_rigidbody2d.velocity.x, _jumpForce * charger);
+            _animator.SetTrigger(_jumpAnimationTriggerName);
+
+            isInAir = true;
+            isJumped = false;
+            charger = 0f;
+        }
+    }
+}
